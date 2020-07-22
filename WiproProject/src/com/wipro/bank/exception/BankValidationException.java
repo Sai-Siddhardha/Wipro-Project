b9d57482e2f2ebe8522de8a77a3e67f5f0346e4c
Sai Siddhartha <Sai Siddhartha@192.168.0.12>
2020-07-22 15:17:19 +0530

    COMMIT

---
+++ WiproProject/src/com/wipro/bank/exception/BankValidationException.java
@@ -0,0 +1,15 @@
+package com.wipro.bank.exception;
+
+public class BankValidationException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String toString(){
+		return "Invalid Data";
+	}
+	public BankValidationException(String str)
+	{
+		super(str);
+	}
+}
